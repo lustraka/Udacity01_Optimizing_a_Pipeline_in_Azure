ea9701631d3a6452a3a786301bf1f08003d30913
Lubomir Straka <58931033+lustraka@users.noreply.github.com>
2021-06-17 11:34:46 +0200

    Update train.py

--- train.py
+++ train.py
@@ -73,6 +73,8 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+    
+    joblib.dump(model, 'model.joblib')
 
 if __name__ == '__main__':
     main()

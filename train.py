d59fc1829017bc7df15226cc05ec5492d7f4705b
Lubomir Straka <58931033+lustraka@users.noreply.github.com>
2021-06-21 07:23:04 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -53,7 +53,7 @@ def get_X_y(ds, encode_cat='onehot'):
 x, y = get_X_y(ds)
 
 # Split data into train and test sets.
-x_train, x_test, y_train, y_test = train_test_split(x, y)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42)
 
 run = Run.get_context()
 
@@ -62,20 +62,20 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument('--max_iter', type=int, default=1000, help="Maximum number of iterations to converge")
 
     args = parser.parse_args()
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
 
-    model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
+    model = LogisticRegression(C=args.C, max_iter=args.max_iter, solver='lbfgs').fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
-    run.log("accuracy", np.float(accuracy))
+    run.log("Accuracy", np.float(accuracy))
     
-    os.makedirs('outputs', exist_ok=True)
-    joblib.dump(model, 'outputs/model.joblib')
+    os.makedirs('./outputs', exist_ok=True)
+    joblib.dump(model, './outputs/model.joblib')
 
 if __name__ == '__main__':
     main()
